dae5842428e4a079c57e35e44c81f4b3f2be178f
Luis Carmona <lcarmona@nyc.yearup.org>
2019-12-02 16:01:11 -0500

    python cover page done

---
+++ pisce.py
@@ -0,0 +1,14 @@
+import turtle
+
+pisces = turtle.Turtle()
+pisces.color('white')
+pisces.screen.bgcolor('black')
+
+pisces.penup()
+pisces.write('Midterm Constellation Project')
+pisces.goto(-7, -42)
+pisces.write('Pisces Constellation')
+pisces.goto(-27, -74)
+pisces.write('Luis Carmona')
+pisces.pendown()
+pisces.hideturtle()
